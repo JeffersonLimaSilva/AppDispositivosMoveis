6aab923721ea64e7209f47e84adea6f89b1f32ed
jeffersonlimasilva <jeffersonsilvasilva142@gmail.com>
2025-09-13 23:03:01 -0300

    Mudanças na ActivityMain - Cadastro de Usuários

--- MoniSaudeMental/app/src/main/java/br/edu/utfpr/alunos/jeffersonlima/monisaudemental/MainActivity.java
+++ MoniSaudeMental/app/src/main/java/br/edu/utfpr/alunos/jeffersonlima/monisaudemental/MainActivity.java
@@ -1,18 +1,50 @@
 package br.edu.utfpr.alunos.jeffersonlima.monisaudemental;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextName, editTextAge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextAge  = findViewById(R.id.editTextAge);
+    }
+
+    public void clearInput(View view){
+        editTextName.setText(null);
+        editTextAge.setText(null);
+
+        editTextName.requestFocus();
+        Toast.makeText(this,
+                R.string.clear_all,
+                Toast.LENGTH_LONG).show();
+    }
+    public void saveValues(View view){
+        String name = editTextName.getText().toString();
+        if(name == null || name.trim().isEmpty()){
+            Toast.makeText(this,
+                    R.string.invalid_value_name,
+                    Toast.LENGTH_LONG).show();
+            editTextName.requestFocus();
+            return;
+        }
+        String age = editTextAge.getText().toString();
+        if(age == null || age.trim().isEmpty()){
+            Toast.makeText(this,
+                    R.string.invalid_value_age,
+                    Toast.LENGTH_LONG).show();
+            editTextName.requestFocus();
+            return;
+        }
+
     }
 }
